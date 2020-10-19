cdd70bf29d582d6102d3f040017a6b715f7b8b4e
andreea-munteanu <57788684+andreea-munteanu@users.noreply.github.com>
2020-10-19 18:01:02 +0300

    Update Lab2.py

--- Lab2-3/Lab2.py
+++ Lab2-3/Lab2.py
@@ -2,8 +2,8 @@ import random
 import sys
 from queue import Queue
 
-n = 20
-m = 30
+n = 12
+m = 15
 print("n = ", n, "m = ", m)
 SIZE = (n, m)  # n rows, m columns
 
@@ -13,61 +13,55 @@ if sys.getrecursionlimit() < SIZE[0] * SIZE[1] :
 # if max recursion limit is lower than needed, adjust it
 sys.setrecursionlimit(30000)
 
-my_list = [0] * 70 + [1] * 30
+my_list = [0] * 55 + [1] * 45
 # initializing labyrinth with 0s and 1s at weighted random
 lab = list(list(random.choice(my_list) for i in range(SIZE[0])) for j in range(SIZE[1]))
 
 
-def print_labyrinth() :
+# parameters: start, destination
+def print_labyrinth(xs, ys, xd, yd) :
     print("    ", end='')
-    for i in range(n):
-        if i < n-1:
+    for i in range(n) :
+        if i < n - 1 :
             print(i, end=' ')
-        else:
+        else :
             print(i)
     print("    ", end='')
-    for i in range(2*n):
+    for i in range(2 * n) :
         print("_", end='')
     print('')
 
     for j in range(m) :
-        if j < 10:
+        if j < 10 :
             print(j, " |", end='')
-        else:
-            print(j, "|", end= '')
+        else :
+            print(j, "|", end='')
         # print("| ", end='')
         for i in range(n) :
-            if lab[j][i] == 0 :
-                print(lab[j][i], end=' ')
+            # start position
+            if i == xs and j == ys :
+                print('S', end=' ')
+            # destination
+            elif i == xd and j == yd :
+                print('D', end=' ')
+            # free cell
+            elif lab[j][i] == 0 :
+                print(' ', end=' ')
+            # obstacle
             else :
                 print('*', end=' ')
         print('|', j, end='\n')
 
-
-print("\nYour labyrinth is: ")
-print_labyrinth()
-print("\n\n")
-print("\nChoose start(xs, ys) and destination (xd, yd): ", end='')
-xs, ys, xd, yd = input().split()
-
-xs = int(xs)
-ys = int(ys)
-xd = int(xd)
-yd = int(yd)
-
-found = False
-
-# (xc, yc) - current position
-# (xs, ys) - start position
-# (xd, yd) - destination
 
 """_________________________________________________________________________________________________________
                                                     1. STATE
           (xc , yc , xs , ys , xd , yd , found)
      ______________ _________ ___________ ___________________________
      current state |  start  |destination| reached final state (bool)
-
-
+    
+    # (xc, yc) - current position
+    # (xs, ys) - start position
+    # (xd, yd) - destination
 
     _________________________________________________________________________________________________________
                               2. INITIALIZATION, INITIAL/FINAL STATE, CHECK_FINAL()
@@ -77,7 +71,7 @@ found = False
 """
 
 
-# 2.1.initialization
+# 2.1.initialization + returning state
 def initialState(xc, yc, xs, ys, xd, yd, found) :
     xc = xs
     yc = ys
@@ -92,7 +86,7 @@ def finalState(xc, yc, xd, yd) :
 
 """_________________________________________________________________________________________________________
                                             3. TRANSITION FUNCTION
-
+                                            
     3.1. validation function (bool)
     3.2. transition function returning current state
    _________________________________________________________________________________________________________
@@ -141,48 +135,50 @@ directions = [N, E, W, S]
    _________________________________________________________________________________________________________
 """
 
-
 BKT_stack = []
 
 
-def print_BKT_path(stack) :
+def print_BKT_path(xs, ys, xd, yd, stack) :
     # stack contains indices (i,j) of the path --> requires step = 2
     for i in range(len(stack), 2) :
         # printing stack for check
         print("(", stack[i], ", ", stack[i + 1], ")", end='; ')
         # marking path with '+'
-        lab[stack[i]][stack[i + 1]] = '+'
+        lab[stack[i]][stack[i + 1]] = '1'
     # printing resulting path
     print("\nBKT path: ")
-    print_labyrinth()
+    print_labyrinth(xs, ys, xd, yd)
     # changing matrix to original
     for i in range(len(stack), 2) :
         lab[stack[i]][stack[i + 1]] = '0'
+
 
 def BKT(xc, yc, xs, ys, xd, yd) :
     BKT_stack.append(xc)
     BKT_stack.append(yc)
     visited.append(xc)
     visited.append(yc)
+    lab[xc][yc] = '1'
     if finalState(xc, yc, xd, yd) :
         print('Found solution: ')
-        print_BKT_path(BKT_stack)
+        print_BKT_path(xs, ys, xd, yd, BKT_stack)
         return True
     else :
         for direction in directions :
             # if transition has been made successfully, move to new position
             sum_x = [GO_DIR[direction][0], xc]
             sum_y = [GO_DIR[direction][1], yc]
-            xc_new = sum(sum_x) # xc_new = GO_DIR[direction][0] + xc
-            yc_new = sum(sum_y)
+
+            xc_new = sum(sum_x)  # xc_new = GO_DIR[direction][0] + xc
+            yc_new = sum(sum_y)  # yc_new = GO_DIR[direction][1] + yc
+
             if valid_transition(xc_new, yc_new) :
                 new_x, new_y = transition(xc_new, yc_new, xc_new, yc_new)
-                BKT(new_x, new_y, xs, ys, xd, yd)
+                return BKT(new_x, new_y, xs, ys, xd, yd)
+    lab[xc][yc] = '0'
     BKT_stack.pop()
     BKT_stack.pop()
 
-
-BKT(xs, ys, xs, ys, xd, yd)
 
 """_________________________________________________________________________________________________________
                                             5. BFS STRATEGY
@@ -190,26 +186,40 @@ BKT(xs, ys, xs, ys, xd, yd)
 """
 
 
-class Point(object):
+class Point(object) :
     def __init__(self, x, y) :
         self.x = x
         self.y = y
 
-def BFS(xc, yc, xs, ys, xd, yd):
+
+def BFS(xc, yc, xs, ys, xd, yd) :
     q = Queue(maxsize=10000)
     position = Point(xc, yc)
     BFS_visited = set()
     BFS_visited.add(position)
     q.put(position)
-    while q:
+    while q :
         u = q.get()
         # calculate neighbours of u, check if they are visited
-        for i in range(4):
+        for i in range(4) :
             # directions N, S, E, W
             print("new line")
 
 
+def main() :
+    found = False
+    print("\nChoose start(xs, ys) and destination (xd, yd): ", end='')
+    xs, ys, xd, yd = input().split()
+    xs = int(xs)
+    ys = int(ys)
+    xd = int(xd)
+    yd = int(yd)
+    lab[xs][ys] = 5
+    lab[xd][yd] = 4
+    print("\nYour labyrinth is: ")
+    print_labyrinth(xs, ys, xd, yd)
+    BKT(xs, ys, xs, ys, xd, yd)
 
 
-
-
+if __name__ == "__main__" :
+    main()
